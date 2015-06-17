cb59b917a5061379e4392fc16d4da6753bb5e192
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-17 17:10:14 +0100

    amazon upload working

--- handlers.js
+++ handlers.js
@@ -1,3 +1,6 @@
+var aws = require('aws-sdk');
+var level = require('level');
+var db = level('./mydb');
 
 function handlers() {
   return {
@@ -18,10 +21,10 @@ function handlers() {
     },
 
     loginUser: function(request, reply) {
-       request.log('analytics request is being sent');
-        request.auth.session.set(request.auth.credentials.profile);
-        reply.redirect('/');
-        //add changed buttons
+      request.log('analytics request is being sent');
+      request.auth.session.set(request.auth.credentials.profile);
+      reply.redirect('/');
+      //add changed buttons
     },
 
     logoutUser: function(request,reply) {
@@ -32,10 +35,10 @@ function handlers() {
 
     awsS3: function(request, reply) {
       console.log("sign s3");
-      aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+      aws.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
       var s3 = new aws.S3();
       var s3_params = {
-        Bucket: S3_BUCKET,
+        Bucket: process.env.S3_BUCKET,
         Key: 'images/' + request.query.file_name,
         Expires: 60,  
         ContentType: request.query.file_type,
@@ -44,16 +47,9 @@ function handlers() {
       s3.getSignedUrl('putObject', s3_params, function(err, data){
         if(err){
           console.log(err);
+        } else {
+          reply(data);
         }
-          else{
-              // var return_data = {
-              //     signed_request: data,
-              //     url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+request.query.file_name
-              // };
-              // reply.write(JSON.stringify(return_data));
-              // reply.end();
-              reply(data);
-          }
       });
     }
   };
